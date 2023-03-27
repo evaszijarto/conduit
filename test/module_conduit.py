21647f3d6a68dfaa6374fcbb79e4f18c6824f819
Szijártó <eva.szijarto.82@gmail.com>
2023-03-27 16:07:17 +0200

    test coding 20230327:1600

--- test/module_conduit.py
+++ test/module_conduit.py
@@ -1,48 +1,48 @@
-from data_conduit import user_data, user
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.relative_locator import locate_with
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from datetime import datetime, date, time, timezone
-
-service = Service(executable_path=ChromeDriverManager().install())
-options = Options()
-options.add_experimental_option("detach", True)
-browser = webdriver.Chrome(service=service, options=options)
-
-URL = 'http://localhost:1667/#/'
-browser.get(URL)
-browser.maximize_window()
-
-
-def cookies_accept():
-    btn_cookies_accept = browser.find_element(By.XPATH,
-                                              '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
-    btn_cookies_accept.click()
-
-
-def sign_up(user_number):
-    btn_menu_sign_up = browser.find_element(By.XPATH, '//a[@href="#/register"]')
-    btn_menu_sign_up.click()
-    input_username = browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
-    input_username.send_keys(user_data['username'] + str(user_number))
-    user.append(user_data['username'] + str(user_number))
-    input_email = browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
-    input_email.send_keys(user_data['username'] + str(user_number) + user_data['email'])
-    input_password = browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
-    input_password.send_keys(user_data['password'])
-    btn_func_sign_up = browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
-    # btn_func_sign_up.click()
-
-
-cookies_accept()
-sign_up(1)
-
-# browser.quit()
+# from data_conduit import user_data, user
+#
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.relative_locator import locate_with
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.wait import WebDriverWait
+# from datetime import datetime, date, time, timezone
+#
+# service = Service(executable_path=ChromeDriverManager().install())
+# options = Options()
+# options.add_experimental_option("detach", True)
+# browser = webdriver.Chrome(service=service, options=options)
+#
+# URL = 'http://localhost:1667/#/'
+# browser.get(URL)
+# browser.maximize_window()
+#
+#
+# def cookies_accept():
+#     btn_cookies_accept = browser.find_element(By.XPATH,
+#                                               '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
+#     btn_cookies_accept.click()
+#
+#
+# def sign_up(user_number):
+#     btn_menu_sign_up = browser.find_element(By.XPATH, '//a[@href="#/register"]')
+#     btn_menu_sign_up.click()
+#     input_username = browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
+#     # input_username.send_keys(user_data['username'] + str(user_number))
+#     # user.append(user_data['username'] + str(user_number))
+#     input_email = browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+#     # input_email.send_keys(user_data['username'] + str(user_number) + user_data['email'])
+#     input_password = browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
+#     # input_password.send_keys(user_data['password'])
+#     btn_func_sign_up = browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+#     # btn_func_sign_up.click()
+#
+#
+# # cookies_accept()
+# # sign_up(1)
+#
+# # browser.quit()
--- test/test_conduit.py
+++ test/test_conduit.py
@@ -10,7 +10,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from datetime import datetime, date, time, timezone
 
+import time
+# from module_conduit import cookies_accept, sign_up
+from data_conduit import user_data, user
+
+
 class TestConduit(object):
+    test_counter = 1
 
     def setup_method(self):
         service = Service(executable_path=ChromeDriverManager().install())
@@ -25,5 +31,35 @@ class TestConduit(object):
     def teardown_method(self):
         self.browser.quit()
 
-    def test_login(self):
-        
+    def test_cookies_accept(self):
+        self.cookie_policy_panel = self.browser.find_element(By.ID, 'cookie-policy-panel')
+        self.btn_cookies_accept = self.browser.find_element(By.XPATH, '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
+        self.btn_cookies_accept.click()
+        assert not self.btn_cookies_accept.is_displayed()
+        # assert not self.cookie_policy_panel.is_enabled()
+
+    def test_sign_up(self):
+        # cookies_accept()
+
+        self.btn_menu_sign_up = self.browser.find_element(By.XPATH, '//a[@href="#/register"]')
+        self.btn_menu_sign_up.click()
+        self.input_username = self.browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
+        self.input_username.send_keys(user_data['username'] + str(self.test_counter))
+        user.append(user_data['username'] + str(self.test_counter))
+        self.input_email = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+        self.input_email.send_keys(user_data['username'] + str(self.test_counter) + user_data['email'])
+        self.input_password = self.browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
+        self.input_password.send_keys(user_data['password'])
+        self.btn_func_sign_up = self.browser.find_element(By.XPATH,
+                                                          '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+        # self.btn_func_sign_up.click()
+        assert self.btn_func_sign_up.is_displayed()
+        assert self.input_username != ""
+        assert self.input_email != ""
+        assert self.input_password != ''
+
+        time.sleep(2)
+        self.test_counter += 1
+        print()
+        print(self.test_counter)
+        print(user)
