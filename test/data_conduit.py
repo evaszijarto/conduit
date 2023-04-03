37c23ab2e0f44a3cf7649a2abd00c89e9d643fa9
Szijártó <eva.szijarto.82@gmail.com>
2023-04-04 00:03:53 +0200

    coding 20230404_0000

--- test/data_conduit.py
+++ test/data_conduit.py
@@ -1,7 +1,14 @@
-user_data = {
-    'username': 'conduit_test_user_01',
-    'email': '@conduit_test_user_01',
+sign_up_user = {
+    'username': 'conduit_test_user_08',
+    'email': '@conduit_test_user_08.com',
     'password': 'Password123'
 }
 
+login_user = {
+    'username': 'conduit_test_user_10',
+    'email': '@conduit_test_user_10.com',
+    'password': 'Password123'
+}
 user = []
+
+btns_menu_expected_text = [" New Article", " Settings", f'{login_user["username"]}', " Log out", "Your Feed" ]
--- test/test_conduit.py
+++ test/test_conduit.py
@@ -9,9 +9,10 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from datetime import datetime, date, time, timezone
+import allure
 
 import time
-from data_conduit import user_data, user
+from data_conduit import sign_up_user, login_user, btns_menu_expected_text
 
 
 class TestConduit(object):
@@ -32,37 +33,99 @@ class TestConduit(object):
     def teardown_method(self):
         self.browser.quit()
 
+    @allure.id('TC1')
+    @allure.title('Oldal megnyitása')
     def test_open(self):
         page_name = self.browser.find_element(By.XPATH, '//a[@class="navbar-brand router-link-exact-active router-link-active"]')
+        logo_name = self.browser.find_element(By.XPATH, '//h1[@class="logo-font"]')
         assert page_name.is_displayed()
+        assert logo_name.is_displayed()
         assert page_name.text == "conduit"
+        assert logo_name.text == "conduit"
 
+    @allure.id('TC2')
+    @allure.title('Adatkezelési nyilatkozat elfogadása')
     def test_cookies_accept(self):
         cookie_policy_panel = self.browser.find_element(By.ID, 'cookie-policy-panel')
         btn_cookies_accept = self.browser.find_element(By.XPATH, '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
         assert cookie_policy_panel.is_enabled()
         assert btn_cookies_accept.is_enabled()
         btn_cookies_accept.click()
-        # assert cookie_policy_panel.get_attribute('class') == ""
-        # assert btn_cookies_accept.get_attribute('class') == ""
+        # time.sleep(2)
+        # assert cookie_policy_panel.get_attribute("value") == ""
+        # assert btn_cookies_accept.get_attribute('value') == ''
 
+
+    @allure.id('TC3')
+    @allure.title('Regisztráció - Helyes adatokkal')
     def test_sign_up(self):
         self.test_cookies_accept()
 
         btn_menu_sign_up = self.browser.find_element(By.XPATH, '//a[@href="#/register"]')
         btn_menu_sign_up.click()
         input_username = self.browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
-        input_username.send_keys(user_data['username'])
-        user.append(user_data['username'])
+        input_username.send_keys(sign_up_user['username'])
         input_email = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
-        input_email.send_keys(user_data['username'] + user_data['email'])
+        input_email.send_keys(sign_up_user['username'] + sign_up_user['email'])
         input_password = self.browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
-        input_password.send_keys(user_data['password'])
+        input_password.send_keys(sign_up_user['password'])
         btn_func_sign_up = self.browser.find_element(By.XPATH,
                                                           '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+
         assert btn_func_sign_up.is_displayed()
         assert input_username != ""
         assert input_email != ""
         assert input_password != ''
+        assert input_username.get_attribute('value') == sign_up_user['username']
+        assert input_email.get_attribute('value') == sign_up_user['username'] + sign_up_user['email']
+        assert input_password.get_attribute('value') == sign_up_user['password']
 
-        # self.btn_func_sign_up.click()
+        btn_func_sign_up.click()
+        time.sleep(3)
+        message_ok = self.browser.find_element(By.XPATH, '//div[@class="swal-text"]')
+        # message_ok = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@class="swal-text"]')))
+        assert message_ok.text == 'Your registration was successful!'
+        # print(message_ok.text)
+        btn_ok_sign_up = self.browser.find_element(By.XPATH, '//button[@class="swal-button swal-button--confirm"]')
+        assert btn_ok_sign_up.is_enabled()
+        btn_ok_sign_up.click()
+
+        self.test_open
+
+    @allure.id('TC4')
+    @allure.title('Bejelentkezés - Helyes adatokkal')
+    def test_login(self):
+        self.test_cookies_accept()
+
+        btn_menu_login = self.browser.find_element(By.XPATH, '//a[@href="#/login"]')
+        btn_menu_login.click()
+        input_email = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+        input_email.send_keys(login_user['username'] + login_user['email'])
+        input_password = self.browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
+        input_password.send_keys(login_user['password'])
+        btn_func_login = self.browser.find_element(By.XPATH,
+                                                     '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+
+        assert btn_func_login.is_displayed()
+        assert input_email != ""
+        assert input_password != ''
+        assert input_email.get_attribute('value') == login_user['username'] + login_user['email']
+        assert input_password.get_attribute('value') == login_user['password']
+
+        btn_func_login.click()
+        # time.sleep(3)
+        # btn_menu_logged_in_user = self.browser.find_element(By.XPATH, '//a[@href="#/@conduit_test_user_10/"]')
+        btn_menu_logged_in_user = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//a[@href="#/@conduit_test_user_10/"]')))
+        assert btn_menu_logged_in_user.is_displayed()
+        assert btn_menu_logged_in_user.is_enabled()
+        assert btn_menu_logged_in_user.text == login_user['username']
+        btns_menu_new = self.browser.find_elements(By.XPATH, '//a[@class="nav-link"]')
+        n = 0
+        for btn in btns_menu_new:
+            # print(btn.text)
+            assert btn.text == btns_menu_expected_text[n]
+            n += 1
+        self.test_open
+
+
+
