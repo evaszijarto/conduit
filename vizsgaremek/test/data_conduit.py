ae64cd03a36b77a07b5c60751af856cfc93caf38
Szijártó <eva.szijarto.82@gmail.com>
2023-04-12 22:45:02 +0200

    Manualis tesztjegyzokonyvek and code refresh 20230412_2245

--- vizsgaremek/test/data_conduit.py
+++ vizsgaremek/test/data_conduit.py
@@ -4,22 +4,16 @@ sign_up_user = {
     'password': 'Password123'
 }
 
-login_user = {
-    'username': 'conduit_test_user_10',
-    'email': '@conduit_test_user_10.com',
-    'password': 'Password123'
-}
-user = []
-
-btns_menu_logged_in_expected_text = [" New Article", " Settings", f'{sign_up_user["username"]}', " Log out", "Your Feed"]
+btns_menu_logged_in_expected_text = [" New Article", " Settings", f'{sign_up_user["username"]}', " Log out",
+                                     "Your Feed"]
 
 btns_menu_logged_out_expected_text = ["Home", "Sign in", "Sign up", "Global Feed"]
 
 new_article_data = {
-        "article_title": "Aenean ac dapibus libero",
-        "article_about": "Aenean ac dapibus libero. 0",
-        "article": "Aenean ac dapibus libero. Vestibulum molestie et est nec accumsan. Fusce venenatis nulla sit amet imperdiet semper. Aliquam non vulputate.",
-        "article_tags": "tag1 tag2"
+    "article_title": "Aenean ac dapibus libero",
+    "article_about": "Aenean ac dapibus libero. 0",
+    "article": "Aenean ac dapibus libero. Vestibulum molestie et est nec accumsan. Fusce venenatis nulla sit amet imperdiet semper. Aliquam non vulputate.",
+    "article_tags": "tag1 tag2"
 }
 
 update_article_data = {
